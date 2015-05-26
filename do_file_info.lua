c3c1969eda6c59531d0f2ccecfd82c7e4b12746d
tigit <m4tigit@gmail.com>
2015-05-27 00:47:53 +0800

    ...
    Signed-off-by: tigit <m4tigit@gmail.com>

--- do_file_info.lua
+++ do_file_info.lua
@@ -1,10 +1,17 @@
 function handle(data)
-    ngx.say(cjson.encode(
-    {
-        ['task/ping.lua'] = { v = 1, s = 254 },
-        ['task/test.lua'] = { v = 1, s = 77 },
-    }
-    ))
+    if 'task/ping.lua' == data then
+        ngx.say(cjson.encode(
+        {
+            ['task/ping.lua'] = { v = 1, s = 254 },
+        }
+        ))
+    else
+        ngx.say(cjson.encode(
+        {
+            ['task/test.lua'] = { v = 1, s = 77 },
+        }
+        ))
+    end
 end
 
 return handle
